e447aede7c8f41229702c666b8d45cdcc5be1c88
DmitriyNepochatov <dima2001nepochatov@gmail.com>
2022-09-15 12:41:30 +0300

    Initialize

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,9 +1,9 @@
-package com.softserve;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
